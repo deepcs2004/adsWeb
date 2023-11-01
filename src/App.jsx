0298829403fbf7ccd04274ce02c805347f2c1079
Deep Pal <112393265+deepcs2004@users.noreply.github.com>
2023-11-01 20:59:24 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './Components/Header/Header'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='text-4xl'>hello</h1>
+
+      
+
     </>
   )
 }
