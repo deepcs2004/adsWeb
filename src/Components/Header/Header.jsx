2ba71525d05a7f174a3259109d4f2056800c522b
Deep Pal <112393265+deepcs2004@users.noreply.github.com>
2023-11-02 23:38:38 +0530

    updated

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -8,7 +8,8 @@ function Header() {
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
           <Link to="/" className="flex items-center">
             <img
-              src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
+              
+              src="https://img.icons8.com/fluency/48/000000/cheap-2.png"
               className="mr-3 h-12"
               alt="Logo"
             />
