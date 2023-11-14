7bdc9e517185ac72559d30a727e4a6b2e272b206
Deep Pal <112393265+deepcs2004@users.noreply.github.com>
2023-11-14 18:45:49 +0530

    updated

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -116,6 +116,119 @@ function Blog() {
             In the digital age, personal branding is essential. This article delves into the strategies for creating and promoting your personal brand online, whether for career advancement or entrepreneurship.
           </p>
         </article>
+
+        <article className="bg-white rounded-lg shadow-md font-mono p-6 mb-8 transform transition-transform hover:scale-105">
+          <h1 className="text-3xl font-semibold text-green-600 mb-4">Investing 101: Building Wealth for the Future</h1>
+          <p className="text-sm text-gray-600 mb-4">Published on November 10, 2023</p>
+          <img
+            src="https://images.pexels.com/photos/271624/pexels-photo-271624.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="Investing Image"
+            className="w-full rounded-lg mb-4"
+          />
+          <p className="text-lg text-gray-800 leading-relaxed mb-4">
+            Discover the basics of investing and learn how to build long-term wealth. This article covers key investment strategies, risk management, and the importance of diversification.
+          </p>
+        </article>
+
+
+        <article className="bg-white rounded-lg shadow-md font-mono p-6 mb-8 transform transition-transform hover:scale-105">
+          <h1 className="text-3xl font-semibold text-blue-600 mb-4">Budgeting Tips for Financial Freedom</h1>
+          <p className="text-sm text-gray-600 mb-4">Published on November 18, 2023</p>
+          <img
+            src="https://images.pexels.com/photos/1620750/pexels-photo-1620750.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="Budgeting Image"
+            className="w-full rounded-lg mb-4"
+          />
+          <p className="text-lg text-gray-800 leading-relaxed mb-4">
+            Master the art of budgeting to take control of your finances. Explore practical tips for creating and sticking to a budget that aligns with your financial goals.
+          </p>
+        </article>
+
+        <article className="bg-white rounded-lg shadow-md font-mono p-6 mb-8 transform transition-transform hover:scale-105">
+          <h1 className="text-3xl font-semibold text-purple-600 mb-4">Navigating Credit Scores: A Guide to Financial Health</h1>
+          <p className="text-sm text-gray-600 mb-4">Published on November 25, 2023</p>
+          <img
+            src="https://images.pexels.com/photos/574071/pexels-photo-574071.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="Credit Score Image"
+            className="w-full rounded-lg mb-4"
+          />
+          <p className="text-lg text-gray-800 leading-relaxed mb-4">
+            Understand the importance of credit scores and learn how to maintain a healthy financial profile. This guide covers tips for improving your credit score and making informed financial decisions.
+          </p>
+        </article>
+
+
+        <article className="bg-white rounded-lg shadow-md font-mono p-6 mb-8 transform transition-transform hover:scale-105">
+          <h1 className="text-3xl font-semibold text-orange-600 mb-4">Side Hustles: Boosting Your Income Streams</h1>
+          <p className="text-sm text-gray-600 mb-4">Published on october 2, 2023</p>
+          <img
+            src="https://images.pexels.com/photos/3408741/pexels-photo-3408741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="Side Hustles Image"
+            className="w-full rounded-lg mb-4"
+          />
+          <p className="text-lg text-gray-800 leading-relaxed mb-4">
+            Explore the world of side hustles and discover practical ways to boost your income. This article provides insights into finding the right side hustle for you and maximizing its potential.
+          </p>
+        </article>
+
+
+        <article className="bg-white rounded-lg shadow-md font-mono p-6 mb-8 transform transition-transform hover:scale-105">
+          <h1 className="text-3xl font-semibold text-teal-600 mb-4">Smart Saving: Strategies for Growing Your Money</h1>
+          <p className="text-sm text-gray-600 mb-4">Published on october 10, 2023</p>
+          <img
+            src="https://images.pexels.com/photos/1676694/pexels-photo-1676694.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="Smart Saving Image"
+            className="w-full rounded-lg mb-4"
+          />
+          <p className="text-lg text-gray-800 leading-relaxed mb-4">
+            Learn effective saving strategies to grow your money over time. This guide covers tips for building an emergency fund, investing in savings accounts, and making your money work for you.
+          </p>
+        </article>
+
+        <article className="bg-white rounded-lg shadow-md font-mono p-6 mb-8 transform transition-transform hover:scale-105">
+          <h1 className="text-3xl font-semibold text-indigo-600 mb-4">Real Estate Investments: A Beginner's Guide</h1>
+          <p className="text-sm text-gray-600 mb-4">Published on october 18, 2023</p>
+          <img
+            src="https://images.pexels.com/photos/540518/pexels-photo-540518.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="Real Estate Image"
+            className="w-full rounded-lg mb-4"
+          />
+          <p className="text-lg text-gray-800 leading-relaxed mb-4">
+            Dive into the basics of real estate investments and discover how to start building wealth through property. This beginner's guide covers key considerations, risks, and potential rewards.
+          </p>
+        </article>
+
+
+        <article className="bg-white rounded-lg shadow-md font-mono p-6 mb-8 transform transition-transform hover:scale-105">
+          <h1 className="text-3xl font-semibold text-red-600 mb-4">Mastering the Art of Negotiation for Financial Success</h1>
+          <p className="text-sm text-gray-600 mb-4">Published on october 28, 2023</p>
+          <img
+            src="https://images.pexels.com/photos/3184612/pexels-photo-3184612.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="Negotiation Image"
+            className="w-full rounded-lg mb-4"
+          />
+          <p className="text-lg text-gray-800 leading-relaxed mb-4">
+            Uncover the secrets of successful negotiation and learn how it can impact your financial success. This article provides practical tips for negotiating salaries, deals, and contracts.
+          </p>
+        </article>
+
+        <article className="bg-white rounded-lg shadow-md font-mono p-6 mb-8 transform transition-transform hover:scale-105">
+          <h1 className="text-3xl font-semibold text-purple-600 mb-4">Cryptocurrency 101: Understanding the Future of Money</h1>
+          <p className="text-sm text-gray-600 mb-4">Published on January 5, 2023</p>
+          <img
+            src="https://images.pexels.com/photos/5702908/pexels-photo-5702908.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="Cryptocurrency Image"
+            className="w-full rounded-lg mb-4"
+          />
+          <p className="text-lg text-gray-800 leading-relaxed mb-4">
+            Explore the world of cryptocurrencies and understand their role in shaping the future of money. This beginner's guide covers the basics of blockchain, Bitcoin, and altcoins.
+          </p>
+        </article>
+
+
+
+
+
       </div>
     </div>
   );
